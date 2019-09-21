db4034a9fba1dcfc3eb54c44c199dfe751dadb28
pearl1991 <pearlmcphee@gmail.com>
2019-09-20 21:22:37 -0400

    Initial Commit

---
+++ twitter-clone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  twitter-clone
+//
+//  Created by Pearl on 9/20/19.
+//  Copyright © 2019 Pearl. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
